64105beeabcc6d5a905701925e0938fb8f6433bd
Michail27 <michail27.03@mail.ru>
2021-12-05 11:47:57 +0300

    Add semantic  methods

---
+++ Version.py
@@ -0,0 +1,47 @@
+class Version:
+    def __init__(self, version):
+        self.version = version
+
+    def __eq__(self, other):
+        print(" __eq__ called")
+        return self.version == other.version
+
+    def __ne__(self, other):
+        print(" __ne__ called")
+        return self.version != other.version
+
+    def __lt__(self, other):
+        print(" __lt__ called")
+        return self.version < other.version
+
+    def __le__(self, other):
+        print('__le__called')
+        return self.version <= other.version
+
+    def __gt__(self, other):
+        print(" __gt__ called")
+        return self.version > other.version
+
+    def __ge__(self, other):
+        print(" __ge__ called")
+        return self.version >= other.version
+
+
+def main():
+    to_test = [
+        ("1.0.0", "2.0.0"),
+        ("1.0.0", "1.42.0"),
+        ("1.2.0", "1.2.42"),
+        ("1.1.0-alpha", "1.2.0-alpha.1"),
+        ("1.0.1b", "1.0.10-alpha.beta"),
+        ("1.0.0-rc.1", "1.0.0"),
+    ]
+
+    for version_1, version_2 in to_test:
+        assert Version(version_1) < Version(version_2), "le failed"
+        assert Version(version_2) > Version(version_1), "ge failed"
+        assert Version(version_2) != Version(version_1), "neq failed"
+
+
+if __name__ == "__main__":
+    main()
