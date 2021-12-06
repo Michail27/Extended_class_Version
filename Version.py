95666d324f7256d81cc8212a590871086dba870a
Michail27 <michail27.03@mail.ru>
2021-12-06 11:54:36 +0300

    Add README file and description Version class

--- Version.py
+++ Version.py
@@ -1,4 +1,7 @@
 class Version:
+    """
+    This class compares different versions of programs
+    """
     def __init__(self, version):
         self.version = self._change_version(version)
 
@@ -57,7 +60,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    print(Version('1.1.3') < Version('2.2.3'))
-    print(Version('1.3.0') > Version('0.3.0'))
-    print(Version('0.3.0b') < Version('1.2.42'))
-    print(Version('1.3.42') == Version('42.3.1'))
+
